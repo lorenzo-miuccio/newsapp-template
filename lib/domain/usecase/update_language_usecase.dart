c24ac3bca39175b6173b88361b9f29c8847853ad
Lorenzo Miuccio <lorenzo.miuccio@sysdata.it>
2023-01-23 22:18:59 +0100

    delete language usecase

--- lib/domain/usecase/update_language_usecase.dart
+++
@@ -1,9 +0,0 @@
-import 'package:news_app/domain/repository/language_repository.dart';
-
-class UpdateLanguageUsecase {
-  final LanguageRepository _languageRepository;
-
-  UpdateLanguageUsecase(this._languageRepository);
-
-  void call(String language) => _languageRepository.updateLanguage(language);
-}
