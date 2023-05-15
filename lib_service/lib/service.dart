7b7026090628a750aa68633667060670d2539efa
Lorenzo Miuccio <lorenzo.miuccio@sysdata.it>
2023-05-15 17:59:08 +0200

    refactor: delete duplicated export

--- lib_service/lib/service.dart
+++ lib_service/lib/service.dart
@@ -3,7 +3,6 @@ library service;
 export 'src/key_value_service_impl.dart';
 export 'src/remote/news_api_retrofit.dart';
 export 'src/remote/news_remote_datasource_impl.dart';
-export 'src/remote/news_api_retrofit.dart';
 export 'src/database/article_dao.dart';
 export 'src/database/article_database.dart';
 export 'src/database/news_local_datasource_impl.dart';
